b7c5656b687ef0e92fbca48f67ee6fb19978f965
Jasojuro <jasyyoan@gmail.com>
2025-01-24 16:04:56 +0100

    Age calculator updated

--- Day3/Day3.py
+++ Day3/Day3.py
@@ -1,18 +1,25 @@
 import random
 
 print("Welcome to our Jangi game")
-print("You will have to guess a number between 0 to 50")
+print("You will have to guess a number between 0 to 10")
 
-a = random.randint(0, 5)
+a = random.randint(0, 10)
 
 x= int(input("Enter the number you think is correct: "))
 
-if x>a:
-    print("your number is bigger")
-    print("The number was",a,". Try again Later")
-elif x<a:
-    print("your number is smaller")
-    print("The number was",a,". Try again Later")
-else:
-    print("Congratulation!!. You Had it corrct")
-    print("The number is ",x)
+while a!=x:
+
+    if x>a:
+        print("your number is bigger")
+        print("Try again Later")
+        continue
+        
+    elif x<a:
+        print("your number is smaller")
+        print("The number was",a,". Try again Later")
+        
+    else:
+
+        print("Congratulation!!. You Had it corrct")
+        print("The number is ",x)
+        break
