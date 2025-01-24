97c21f69c37882fed2892b5e71249b1e5d3de7c4
Jasojuro <jasyyoan@gmail.com>
2025-01-24 14:47:01 +0100

    Number-guessing game

--- Day3/Day3.py
+++ Day3/Day3.py
@@ -1,2 +1,18 @@
-print("You will have to guess a number")
-print("you have 3 trials")
+import random
+
+print("Welcome to our Jangi game")
+print("You will have to guess a number between 0 to 50")
+
+a = random.randint(0, 5)
+
+x= int(input("Enter the number you think is correct: "))
+
+if x>a:
+    print("your number is bigger")
+    print("The number was",a,". Try again Later")
+elif x<a:
+    print("your number is smaller")
+    print("The number was",a,". Try again Later")
+else:
+    print("Congratulation!!. You Had it corrct")
+    print("The number is ",x)
