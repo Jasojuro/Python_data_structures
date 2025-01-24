29fcae6a9b3b6b9667f305a940b687cacf6aee03
Jasojuro <jasyyoan@gmail.com>
2025-01-24 21:57:21 +0100

    Number-guessing Game

--- Day3/Day3.py
+++ Day3/Day3.py
@@ -5,12 +5,14 @@ print("You will have to guess a number between 0 to 10")
 a = random.randint(0, 10)
 
 i=0
-while i<2:
+while i<3:
     x= int(input("Enter the number you think is correct: "))
     if x==a:
         print("Congratulation!!. You Had it corrct")
         break
     else:
         print("Your number is incorrect")
-        print(a)
+        
         i+=1
+if i==3:
+    print("The numebr was",a)
