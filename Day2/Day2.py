e4e4fe50c70adf60639c60f3516603f02d819e81
Jasojuro <jasyyoan@gmail.com>
2025-01-24 18:34:08 +0100

    Age calculator Updated

--- Day2/Day2.py
+++ Day2/Day2.py
@@ -1,9 +1,10 @@
 #Here the user enters his or her name
 name= input("What is your name: ") 
 
-age= int(input("Enter your age in years: "))
+age= int(input("Enter your age: "))
 
 #the conversion fron year to days lies on this operation
 Age = age*365 
+
 print("My Name is",name,"and I am",Age,"days old") 
 
