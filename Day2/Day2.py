576e5253cba34850e4d260c0a32836dbe09226bf
Jasojuro <jasyyoan@gmail.com>
2025-01-24 18:25:26 +0100

    Number-guessing Game

--- Day2/Day2.py
+++ Day2/Day2.py
@@ -1,6 +1,9 @@
-name= input("What is your name: ") # Here the user enters his or her name
-age= float(input("Enter your age in years: "))
+#Here the user enters his or her name
+name= input("What is your name: ") 
 
-Age = age*365 #the conversion fron year to days lies on this operation
+age= int(input("Enter your age in years: "))
 
+#the conversion fron year to days lies on this operation
+Age = age*365 
 print("My Name is",name,"and I am",Age,"days old") 
+
--- Day3/Day3.py
+++ Day3/Day3.py
@@ -2,24 +2,15 @@ import random
 
 print("Welcome to our Jangi game")
 print("You will have to guess a number between 0 to 10")
-
 a = random.randint(0, 10)
 
-x= int(input("Enter the number you think is correct: "))
-
-while a!=x:
-
-    if x>a:
-        print("your number is bigger")
-        print("Try again Later")
-        continue
-        
-    elif x<a:
-        print("your number is smaller")
-        print("The number was",a,". Try again Later")
-        
-    else:
-
+i=0
+while i<2:
+    x= int(input("Enter the number you think is correct: "))
+    if x==a:
         print("Congratulation!!. You Had it corrct")
-        print("The number is ",x)
         break
+    else:
+        print("Your number is incorrect")
+        print(a)
+        i+=1
