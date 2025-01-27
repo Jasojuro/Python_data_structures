98ac0ea4ae669c51d959432540b90724b2648a48
Jasojuro <jasyyoan@gmail.com>
2025-01-27 17:43:55 +0100

    Multiplication Table

--- Day4/Day4.py
+++ Day4/Day4.py
@@ -6,4 +6,13 @@ while True:
     else:
         x=int(x)
 
-       
+        for i in range(1,16,1):
+                
+            y=i*x
+            print(i,"*",x,"=",y)
+        choice = input("Do you want to exit the code, Enter yes or no: ")
+        if choice == "yes":
+            print("Thank see you next time")
+            break
+        else: 
+            continue
