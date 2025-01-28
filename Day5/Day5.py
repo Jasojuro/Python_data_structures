801c2c379fa7d761672144c748d122b9c3b3486b
Jasojuro <jasyyoan@gmail.com>
2025-01-28 19:53:40 +0100

    Math operation functions

--- Day5/Day5.py
+++ Day5/Day5.py
@@ -1,4 +1,4 @@
-
+#Addition
 def add():
     total=0
     while True:
@@ -9,10 +9,10 @@ def add():
             number=float(u_input)
             total += number
         except ValueError:
-            input("Enter an interger: ")
+            input("Enter a number: ")#This message is diplayed if a number is not entered
     return total
 
-
+#Subtraction
 def sub():
     total=None
     while True:
@@ -26,9 +26,11 @@ def sub():
             else:
                 total -= number
         except ValueError:
-            input("Enter an number: ")
+            input("Enter a number: ")#This message is diplayed if a number is not entered
     return total
 
+
+#multiplication
 def mul():
     total=None
     while True:
@@ -42,7 +44,7 @@ def mul():
             else:
                 total *= number
         except ValueError:
-            input("Enter an number: ")
+            input("Enter a number: ")#This message is diplayed if a number is not entered
     return total
 
 def div():
@@ -50,26 +52,30 @@ def div():
     while True:
         u_input = input("Enter a number to divide ( type 'done' to stop):  ")#permits the user to enter all the needed to be added until "done" is entered
         if u_input=="done":
+            if total is None: #No number was entered
+                return 0
             break
         try:
             number=float(u_input)
-            if total is None:
+            if total is None: #To assign the first number total
                 total=number
             else:
-                if number==0:
-                    raise TypeError("Division by 0 is not possible")
+                if number ==0:
+                    raise ZeroDivisionError("Division by 0 is not possible.") #Displays this message if 0 is inputed as second number and above
                 total /= number
         except ValueError:
-            input("Enter an number: ")
+            input("Enter a number: ") #This message is diplayed if a number is not entered
+        except ZeroDivisionError as i:
+           print(i)
         
-            
+
     return total
 
 print("Enter 1 for Addition, 2 for subtraction, 3 for multiplication,4 for division ")
-Operation = input("What operation do you want to perform?: ")
+Operation = input("What operation do you want to perform?: ") #Coice of the operation to be performed
 
 match Operation:
-    case "1":
+    case "1": 
         sum=add()
         print(f"The sum of the numbers is : {sum}")
     case "2":
