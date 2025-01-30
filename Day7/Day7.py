141c4951f01596267232234427867c4082f686ee
Jasojuro <jasyyoan@gmail.com>
2025-01-31 00:25:40 +0100

    Contact book program

--- Day7/Day7.py
+++ Day7/Day7.py
@@ -3,23 +3,23 @@ def contact():
     while True:
         print("\n*** Your Contact List ***")
         print("1. Add a Contact")
-        print("2. View a COntact")
+        print("2. View a Contact")
         print("3. Exit")
         choice = input("Enter your choice (1 or 3): ")
 
         if choice == "1":
             name = input("Enter the name associated to the contact to be added: ")
             cont =input("Enter the contact to add: ")
-            dict[name]=cont
+            dict[name]=cont #the name and the contact are being added to the dictionary
         
         elif choice == "2":
              if not dict:
-                print("Your contact list is empty.")
+                print("Your contact list is empty.") #If dictionary is empty returns "Your contact list is empty"
 
              else:
                 name = input("Enter the name associated to the contact to retrieve: " )
                 print("\n*** This is the contact ***")
-                print(f"{name}----{dict.get(name)}")
+                print(f"{name}----{dict.get(name)}")#Displays the name(key) and the contact(value) associated to it.
 
         elif choice == "3":
             print("Thank you for using the contact book. Goodbye!")
